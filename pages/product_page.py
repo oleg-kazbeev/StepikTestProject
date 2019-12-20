16f06017b00fc59d6f6d47250c4b397d47bdc8b6
Казбеев Олег <kazbeev@gmail.com>
2019-12-20 16:01:35 +0700

    Product_page was created which contains class ProductPage with 3 checks (1.Existence of add to basket btn; 2.Existence product name in message about adding product. 3.Equality of product price and basket total message) and 1 method (Add product to basket).

---
+++ pages/product_page.py
@@ -0,0 +1,33 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def should_be_add_to_basket_btn(self):
+        assert self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BTN), "There is no add to basket form"
+
+    def add_product_to_basket(self):
+        add_to_basket_form = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BTN)
+        add_to_basket_form.click()
+
+    def should_be_product_in_basket(self):
+        self.should_be_message_about_adding_product()
+        self.should_be_message_about_basket_total()
+
+    def should_be_message_about_adding_product(self):
+        assert self.browser.find_element(*ProductPageLocators.PRODUCT_NAME), (
+            "Product name is not presented")
+        assert self.browser.find_element(*ProductPageLocators.MESSAGE_ABOUT_ADDING), (
+            "Message about adding is not presented")
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        message_about_adding = self.browser.find_element(*ProductPageLocators.MESSAGE_ABOUT_ADDING).text
+        assert product_name in message_about_adding, "Product name is not presented in message about adding"
+
+    def should_be_message_about_basket_total(self):
+        assert self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE), (
+            "Product price is not presented")
+        assert self.browser.find_element(*ProductPageLocators.MESSAGE_BASKET_TOTAL), (
+            "Basket total message is not presented")
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        basket_total_message = self.browser.find_element(*ProductPageLocators.MESSAGE_BASKET_TOTAL).text
+        assert product_price in basket_total_message, "Basket total message is not equal product price"
