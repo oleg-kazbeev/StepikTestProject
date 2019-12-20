7c03ea9d5ec2f255b25d295387be60c7f8a95f4c
Казбеев Олег <kazbeev@gmail.com>
2019-12-20 16:06:54 +0700

    1. base_page.py: solve_quiz_and_get_code method was added, also was changed timeout parameter (from 10s to 5s) in __init__; 2. locators.py: locators for product_page were added.

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,14 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators():
+    ADD_TO_BASKET_BTN = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
+    MESSAGE_ABOUT_ADDING = (By.CSS_SELECTOR, "div.alertinner ")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    MESSAGE_BASKET_TOTAL = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
+
+
+
